3d387ff56317ba43bb249d90e64a85a87f9bb863
mohamed.hammam <mohamed.hammam@wecancity.org>
2023-10-16 18:38:19 +0300

    Initial Commit

---
+++ MrS-Cool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MrS-Cool
+//
+//  Created by wecancity on 16/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MrS-Cool/MrS_CoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrS_CoolApp.swift
+//  MrS-Cool
+//
+//  Created by wecancity on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MrS_CoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
